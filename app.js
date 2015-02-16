0e449257548be9d09f7e80b5e9adcd39f8d652ff
Domenic Murtari <domenic.murtari@gmail.com>
2015-02-15 19:15:18 -0700

    Added mongo and app framework

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require('express')
+var app = express()
+
+var _ = require('lodash')
+
+var events = require('./data/events.json')
+var issues = require('./data/issues.json')
+
+// use jade as the view engine
+app.set('view engine', 'jade');
+// Hosting
+// set where the static contents are (e.g., css, js)
+app.use(express.static(__dirname + '/public'));
+
+app.get('/list/events', function(req, res) {
+    res.render('listEvents.jade', {
+        events: events
+    })
+})
+
+app.get('/list/issues', function(req, res) {
+    res.render('listIssues.jade', {
+        issues: issues
+    })
+})
+
+app.get('/view/event/:event_id', function(req, res) {
+    // TODO: lookup a event by a event_id
+    // hint: use lodash's find function to look up a event by event_id
+    var event = _.find(events, {'event_id' : req.params.event_id})
+    res.render('viewEvent.jade', {
+        event: event
+    })
+})
+
+app.get('/view/issue/:issue_id', function(req, res) {
+    var issue = _.find(issues, {'issue_id' : req.params.issue_id})
+    res.render('viewIssue.jade', {
+        issue: issue
+    })
+})
+
+var server = app.listen(process.env.PORT || 3000, function() {
+require('./mongo')(app)
+
+var server = app.listen(3000, function() {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('App listening at http://%s:%s', host, port)
+})
